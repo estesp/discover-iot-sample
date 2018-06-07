c037bb609356d5e6bb4b5a9d94b16f158aca0e55
Phil Estes <estesp@gmail.com>
2018-06-07 13:44:00 -0400

    JavaScript cleanup
    Prettier run against JavaScript code
    
    Signed-off-by: Phil Estes <estesp@gmail.com>

--- cloudant/delete-all-docs.js
+++ cloudant/delete-all-docs.js
@@ -1,25 +1,28 @@
 // delete all docs in Cloudant
-var Cloudant = require('cloudant')
+var Cloudant = require("cloudant");
 
 var dbname = process.env.cloudant_db;
-Cloudant({url: process.env.cloudant_url}, function(err, cloudant) {
+Cloudant({ url: process.env.cloudant_url }, function(err, cloudant) {
   if (err)
-    return console.log('Error connecting to Cloudant account %s: %s', url, err.message)
+    return console.log(
+      "Error connecting to Cloudant account %s: %s",
+      url,
+      err.message
+    );
 
   // specify the database we are going to use
-  var ldb = cloudant.db.use(dbname)
+  var ldb = cloudant.db.use(dbname);
   ldb.list(function(err, body) {
-	if (err) 
-	    return console.log('Error listing docs')
-	body.rows.forEach(function(doc) {
-		console.log('deleting id: %s, rev: %s', doc.id, doc.value.rev)
-		ldb.destroy(doc.id, doc.value.rev, function(err, body) {
-		    if (err) console.log('ERROR: %s', err)
-			else console.log(body)
-		})
-	})
-  })
-})
+    if (err) return console.log("Error listing docs");
+    body.rows.forEach(function(doc) {
+      console.log("deleting id: %s, rev: %s", doc.id, doc.value.rev);
+      ldb.destroy(doc.id, doc.value.rev, function(err, body) {
+        if (err) console.log("ERROR: %s", err);
+        else console.log(body);
+      });
+    });
+  });
+});
 
 // can wrap with this if to not delete design docs:
 // if ( doc.id.substring(0,7) != '_design') {
--- dashboard/dataviews.js
+++ dashboard/dataviews.js
@@ -1,62 +1,71 @@
 /* Retrieve cloudant information */
 
-var Cloudant = require('cloudant');
+var Cloudant = require("cloudant");
 
 // cloudant credentials URL
 var cURL = process.env.cloudant_url;
-var cloudant = Cloudant({ url: cURL, plugin: 'promises' });
+var cloudant = Cloudant({ url: cURL, plugin: "promises" });
 var shakeDb = cloudant.db.use(process.env.cloudant_db);
 
 function descendingTotal(a, b) {
-    if (a.value > b.value) {
-        return -1;
-    } else if (b.value > a.value) {
-        return 1;
-    } else {
-        return 0;
-    }
+  if (a.value > b.value) {
+    return -1;
+  } else if (b.value > a.value) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 var lastSeq = "now";
 
 module.exports = {
-    deviceTotals: function(req, res) {
-        var resp = [];
-        shakeDb.view("energyView", "energySum", {
-            'group': true,
-            'reduce': true
-          }, function(err, body) {
-            if (!err) {
-                resp = body.rows
-                resp.sort(descendingTotal);
-                res.send(resp);
-            }
-        });
-    },
-    energyTotals: function(req, res) {
-        // QUERY: _changes?filter=_view&view=totalView/totals&include_docs=true&since=<lastID>
-        // We want to use this kind of query to find recent additions to the view with more data
-        // points. Will have to store last change seq ID and call with it each time.
-        cloudant.db.changes(process.env.cloudant_db, {
-            'filter': '_view',
-            'view': 'totalView/totals',
-            'include_docs': true,
-            'since': lastSeq
-        }, function(err, body) {
-            console.log("last seq: "+body.last_seq);
-            lastSeq = body.last_seq;
-            if (body.results.length > 0) {
-                res.send(body.results);
-            } else {
-                res.send({});
-            }
-        });
-    }
+  deviceTotals: function(req, res) {
+    var resp = [];
+    shakeDb.view(
+      "energyView",
+      "energySum",
+      {
+        group: true,
+        reduce: true
+      },
+      function(err, body) {
+        if (!err) {
+          resp = body.rows;
+          resp.sort(descendingTotal);
+          res.send(resp);
+        }
+      }
+    );
+  },
+  energyTotals: function(req, res) {
+    // QUERY: _changes?filter=_view&view=totalView/totals&include_docs=true&since=<lastID>
+    // We want to use this kind of query to find recent additions to the view with more data
+    // points. Will have to store last change seq ID and call with it each time.
+    cloudant.db.changes(
+      process.env.cloudant_db,
+      {
+        filter: "_view",
+        view: "totalView/totals",
+        include_docs: true,
+        since: lastSeq
+      },
+      function(err, body) {
+        console.log("last seq: " + body.last_seq);
+        lastSeq = body.last_seq;
+        if (body.results.length > 0) {
+          res.send(body.results);
+        } else {
+          res.send({});
+        }
+      }
+    );
+  }
 };
 
 function isEmpty(obj) {
-    if (obj === undefined) {
-        return true;
-    }
-    return Object.keys(obj).length === 0;
+  if (obj === undefined) {
+    return true;
+  }
+  return Object.keys(obj).length === 0;
 }
